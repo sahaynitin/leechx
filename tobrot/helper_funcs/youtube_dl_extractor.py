41c3b889beccc8a00765035bf928225372e8d888
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2022-01-09 14:09:47 +0530

    Update youtube_dl_extractor.py

--- tobrot/helper_funcs/youtube_dl_extractor.py
+++ tobrot/helper_funcs/youtube_dl_extractor.py
@@ -16,7 +16,7 @@ async def extract_youtube_dl_formats(
     url, cf_name, yt_dl_user_name, yt_dl_pass_word, user_working_dir
 ):
     command_to_exec = [
-        "yt-dlp",
+        "yt_dlp==2021.12.27",
         "--no-warnings",
         "--youtube-skip-dash-manifest",
         "--no-check-certificate",
